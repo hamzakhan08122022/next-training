86724deec4d9da1d202d9563dd0c645a3cb8b47f
Hamza Khan <hk@cloudify.biz>
2023-06-06 10:51:49 +0530

    feat: trpc setup and use case

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,9 @@
-"use client"
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import axios from 'axios';
+import ListUsers from "./ListUser";
 
-interface Post {
-  id: number;
-  title: string;
-}
-
-const HomePage = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await axios.get('/api/posts');
-        const postsData: Post[] = response.data;
-        setPosts(postsData);
-      } catch (error) {
-        console.error('Error fetching posts:', error);
-      }
-    };
-    fetchPosts();
-  }, []);
-
+export default function Home() {
   return (
-    <div>
-      <h1>Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Link href={`/posts/${post.id}`}>
-              <div className="post-link">{post.title}</div>
-            </Link>
-          </li>
-        ))}
-      </ul>
-
-      <style jsx>{`
-        .post-link {
-          color: blue;
-          cursor: pointer;
-          text-decoration: underline;
-        }
-      `}</style>
-    </div>
+    <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
+      <ListUsers />
+    </main>
   );
-};
-
-export default HomePage;
+}
